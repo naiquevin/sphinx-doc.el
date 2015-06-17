c6f3053b83eab029c13e680b8200c704880727b1
Vineet Naik <vineet@helpshift.com>
2015-06-18 00:58:58 +0530

    Improve the run-tests.sh script
    1. To work on Mac OS X along with Linux
    2. Add the dependency dirs for latest version of 's' and 'dash' to path

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-emacs -batch \
+set -e
+
+if [[ $(uname -s) == "Darwin" ]]; then
+    EMACS_BIN=/Applications/Emacs.app/Contents/MacOS/Emacs
+else
+    EMACS_BIN=emacs
+fi
+
+function elpa_dep_dir() {
+    echo $(find ~/.emacs.d/elpa -type d -depth 1 | grep -E "/$1-\d+\.\d+" | sort -r | head -n 1)
+}
+
+$EMACS_BIN -batch \
       -L . \
-      -L ~/.emacs.d/elpa/s-20131223.944 \
+      -L $(elpa_dep_dir "dash") \
+      -L $(elpa_dep_dir "s") \
       -l ert \
       -l ./sphinx-doc-tests.el \
       -f ert-run-tests-batch-and-exit
