f19afa379e8ad642a9e1313945e016c6754485a3
Aethor <oow.autre@gmail.com>
2020-11-28 14:21:03 +0100

    Modified test running script for Linux compatibility

--- run-tests.sh
+++ run-tests.sh
@@ -9,7 +9,11 @@ else
 fi
 
 function elpa_dep_dir() {
-    echo $(find ~/.emacs.d/elpa -type d -depth 1 | grep -E "/$1-\d+\.\d+" | sort -r | head -n 1)
+    if [[ $(uname -s) == "Darwin" ]]; then
+	echo $(find ~/.emacs.d/elpa -type d -depth 1 | grep -E "/$1-\d+\.\d+" | sort -r | head -n 1)
+    else
+	echo $(find ~/.emacs.d/elpa -maxdepth 1 -type d | grep -E "/$1-[0-9]+\.[0-9]+" | sort -r | head -n 1)
+    fi
 }
 
 $EMACS_BIN -batch \
